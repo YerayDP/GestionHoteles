c6630aabaf357042606ee63edb7f025a10645e3d
YerayDP <yerayd99@gmail.com>
2021-10-05 09:31:53 +0200

    Proyecto

--- GestionVet/src/main/java/com/example/demo/repository/CitasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.Citas;
-
-public interface CitasRepository extends JpaRepository<Citas, Integer> {
-
-}
